60dc6860c2b8de77edb8d1371557b8f6a475197b
“PJ <YOUR@EMAIL.com>
2016-01-11 02:07:34 +0000

    reset db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110233705) do
+ActiveRecord::Schema.define(version: 20160111015743) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
