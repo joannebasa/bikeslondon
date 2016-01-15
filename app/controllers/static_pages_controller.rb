7f130de513b46c0c0eea5d65546a7096e9d9bc66
“PJ <YOUR@EMAIL.com>
2016-01-15 16:41:07 +0000

    revised 4.2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,8 @@
 class StaticPagesController < ApplicationController
 
  def index
- 	@products = Product.all
  end
 
   def landing_page
-  	@featured_product = Product.limit(3)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Product.create(name: 'Brompton Type S', description: 'Fast, Fun and FOLDING!', image_url: 'type s.jpg', price: '945.00')
+Product.create(:name => "Brompton Type S", :description => "Fast, Fun and FOLDING!", :image_url => "type s.jpg", :price => "945.00")
 
-Product.create(name: 'Brompton Type M', description: 'A Timeless British Classic', image_url: 'type m.jpg', price: '970.00')
+Product.create(:name => "Brompton Type M", :description => "A Timeless British Classic", :image_url => "type m.jpg", :price => "970.00")
 
-Product.create(name: 'Brompton Type H', description: 'Timeless British Classic--but TALLER', image_url: 'type h.jpg', price: '1070.00')
+Product.create(:name => "Brompton Type H", :description => "Timeless British Classic--but TALLER", :image_url => "type h.jpg", :price => "1070.00")
 
-Product.create(name: 'Brompton Type P', description: '1 bike + 2 handlebar positions = FUN!', image_url: 'type p.jpg', price: '1180.00')
+Product.create(:name => "Brompton Type P", :description => "1 bike + 2 handlebar positions = FUN!", :image_url => "type p.jpg", :price => "1180.00")
