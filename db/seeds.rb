d409c3ef9d2d122ba7d76228499beddd4c6a77ba
“PJ <YOUR@EMAIL.com>
2016-01-15 15:48:23 +0000

    revised 4.4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+# ruby encoding: utf-8
 Product.create(name: 'Brompton Type S', description: 'Fast, Fun and FOLDING!', image_url: 'type s.jpg', price: '945.00')
 
 Product.create(name: 'Brompton Type M', description: 'A Timeless British Classic', image_url: 'type m.jpg', price: '970.00')
