dbd71df248cfd90565a12b585f48f34f91f7ad38
“PJ <YOUR@EMAIL.com>
2016-01-28 21:46:28 +0000

    unfinished 5.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
 
@@ -19,6 +21,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
 
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
