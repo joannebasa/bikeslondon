faabaf4bff10885dc218e655eec506256aa37944
“PJ <YOUR@EMAIL.com>
2016-01-31 12:19:45 +0000

    added edit link to nav 5.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
+  after_create :create_profile
+
+  def create_profile
+    self.profile.create
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	has_many :orders
+  has_many :orders
+  
 end
