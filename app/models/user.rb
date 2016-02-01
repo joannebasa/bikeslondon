81a303e8606107324a9704890d2ebafb45040d9b
“PJ <YOUR@EMAIL.com>
2016-02-01 19:34:45 +0000

    revised 5.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ActiveRecord::Base
-  after_create :create_profile
 
-  def create_user
-    self.user.create
-  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
