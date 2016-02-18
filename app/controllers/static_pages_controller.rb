91f95ca7cf02f96717879cdcf7090fbd15a2e9f7
“PJ <YOUR@EMAIL.com>
2016-02-18 16:29:23 +0000

    mail 5.11

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,16 @@
 class StaticPagesController < ApplicationController
   
   def index
-  	
+     if params[:q]
+      search_term = params[:q]
+      @product = Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      @product = Product.all
+    end
   end
 
   def landing_page
-  	@products = Product.limit(3)
+    @products = Product.limit(3)
   end
 
   def thank_you
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -9,14 +9,4 @@ class ApplicationMailer < ActionMailer::Base
           :subject => "A new contact form message from #{name}")
   end
 
-  def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-    ActionMailer::Base.mail(:from => @email,
-        :to => 'pjbasa@gmail.com',
-        :subject => "A new contact form message from #{@name}",
-        :body => @message).deliver_now
-  end
-
 end
