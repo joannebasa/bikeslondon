09abf072c9c2c0d74bbbddeee1c08c2bc32c69b8
“PJ <YOUR@EMAIL.com>
2016-02-18 16:11:44 +0000

    mail 5.11

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -13,7 +13,10 @@ class ApplicationMailer < ActionMailer::Base
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver_now
+    ActionMailer::Base.mail(:from => @email,
+        :to => 'pjbasa@gmail.com',
+        :subject => "A new contact form message from #{@name}",
+        :body => @message).deliver_now
   end
 
 end
