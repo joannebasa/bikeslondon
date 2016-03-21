d0db226078a3a1d1e6a2b0138f05b763fb751d38
“PJ <YOUR@EMAIL.com>
2016-03-21 15:03:03 +0000

    6.7 v2

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,24 +1,21 @@
-def create
-  @product = params [:product_id]
-  token = params[:stripeToken]
-  # Create the charge on Stripe's servers - this will charge the user's card
-  begin
-    charge = Stripe::Charge.create(
-      :amount => "<%= @product.price %>", # amount in cents, again
-      :currency => "gbp",
-      :source => token,
-      :description => params[:stripeEmail]
-    )
-
-    if charge.paid
-      Order.create(:product_id, :user_id, :total)
-    end
-
+class PaymentsController < ApplicationController
+  def create
+    token = params[:stripeToken]
+    product = Product.find(params[:product_id])
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => 2000, # amount in cents, again
+        :currency => "gbp",
+        :source => token,
+        :description => params[:stripeEmail]
+      )
     rescue Stripe::CardError => e
-    # The card has been declined
-    body = e.json_body
-    err = body[:error]
-    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+    redirect_to products_path (product)
   end
-  redirect_to product_path(@product)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217131042) do
+ActiveRecord::Schema.define(version: 20160321144032) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
