f9924419a18590343680a56af4770fa4efcacd0b
“PJ <YOUR@EMAIL.com>
2016-03-31 15:35:22 +0100

    6.12 v1

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,7 @@
 $(document).on('ready page:load', function(){
     $('.rating').raty( { path: '/assets/images-raty', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets/images-raty',
-      readOnly: false,
+      readOnly: true,
       score: function() {
         return $(this).attr('data-score');
       }
