509f99ebb4803cb7c2bf2aa7e49d89af7f84a587
“PJ <YOUR@EMAIL.com>
2016-04-01 14:23:17 +0100

    6.15 v2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,6 +3,8 @@ if Rails.env.development?
   $redis = Redis.new(:host => 'localhost', :port => 6379)
 else 
   Rails.env.production?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  ENV["REDISTOGO_URL"] ||= 'redis://localhost:6379'
+uri = URI.parse(ENV["REDISTOGO_URL"])
+$redis = REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 end 
