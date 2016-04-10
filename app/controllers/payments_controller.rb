ea721099cd5662d2a41e7ad8a6d86659da93bbea
“PJ <YOUR@EMAIL.com>
2016-04-10 17:10:47 +0100

    6.16 v12

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,7 @@
 class PaymentsController < ApplicationController
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
   def create
+    Stripe.api_key = Rails.configuration.stripe[:secret_key]
     token = params[:stripeToken]
     @product = Product.find(params[:product_id])
     @user = current_user
