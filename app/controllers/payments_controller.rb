ddc7ba6154d9e2911cef2d9aa07ca001aa522ec2
“PJ <YOUR@EMAIL.com>
2016-04-10 16:54:07 +0100

    6.16 v11

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,29 +1,28 @@
 class PaymentsController < ApplicationController
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+  def create
+    token = params[:stripeToken]
+    @product = Product.find(params[:product_id])
+    @user = current_user
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => (@product.price*100).to_i, # amount in cents, again
+        :currency => "gbp",
+        :source => token,
+        :description => params[:stripeEmail]
+      )
 
-def create
-  token = params[:stripeToken]
-  @product = Product.find(params[:product_id])
-  @user = current_user
-  # Create the charge on Stripe's servers - this will charge the user's card
-  begin
-    charge = Stripe::Charge.create(
-      :amount => (@product.price*100).to_i, # amount in cents, again
-      :currency => "gbp",
-      :source => token,
-      :description => params[:stripeEmail]
-    )
-
-    if charge.paid
-      Order.create(:product_id => @product_id,:user_id => current_user, :total => @product.price)
+      if charge.paid
+        Order.create(:product_id => @product_id,:user_id => current_user, :total => @product.price)
+      end
+      flash[:success] = "Payment processed successfully"
+    rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+      err  = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
-    flash[:success] = "Payment processed successfully"
-  rescue Stripe::CardError => e
-    # The card has been declined
-    body = e.json_body
-    err  = body[:error]
-    flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    redirect_to product_path(@product)
   end
-  redirect_to product_path(@product)
-end
-
 end
