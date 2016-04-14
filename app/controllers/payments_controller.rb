944facca5fd2e06d483e5697a740a6d9ae3cb73f
“PJ <YOUR@EMAIL.com>
2016-04-14 15:59:15 +0100

    6.16 v42

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -15,8 +15,8 @@ class PaymentsController < ApplicationController
 
     if charge.paid
       Order.create(:product_id => @product_id,:user_id => current_user, :total => @product.price)
-      UserMailer.order_confirmation(@order, @user).deliver_now
     end
+    UserMailer.order_confirmation(@order, @user).deliver_now
     flash[:success] = "Payment processed successfully"
   rescue Stripe::CardError => e
     # The card has been declined
