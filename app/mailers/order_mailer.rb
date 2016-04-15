0bf8d41e217f63ed83c5cb3fccc86cb6ea8b891c
“PJ <YOUR@EMAIL.com>
2016-04-15 14:38:43 +0100

    6.16 v52

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,6 @@ class OrderMailer < ApplicationMailer
   def order_confirmation(user, order)
     @user = user
     @order = order
-    mail(to: user.email, subject: 'Order has been received').deliver_now
+    mail(to: user.email, subject: 'Order has been received')
   end
 end
