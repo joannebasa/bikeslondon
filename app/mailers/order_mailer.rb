0f9e3368c708f80b818039ec64d65e241eabc3c2
“PJ <YOUR@EMAIL.com>
2016-04-15 11:14:45 +0100

    6.16 v45

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,11 +2,8 @@ class OrderMailer < ApplicationMailer
   default from: 'pjbasa@gmail.com'
 
   def order_confirmation(user, email)
-  	@name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
     @user = current_user
-    mail( :to => user.email,
+    mail( :to => @user.email,
         :subject => "#{@appname} Order Received")
   end
 end
